3b8614a6b89e858fd5aa933562d71afe083935f7
DLBell217 <157739853+DLBell217@users.noreply.github.com>
2024-03-05 14:37:39 -0500

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -6,39 +6,48 @@ import FormLabel from '@mui/joy/FormLabel';
 import FormHelperText from '@mui/joy/FormHelperText';
 import Input from '@mui/joy/Input';
 
-
 export default function BasicButtons() {
   return (
+    <Box>
+    
     <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
       <Button>Button</Button>
       <Button disabled>Disabled</Button>
       <Button loading>Loading</Button>
-      <Button>Click me</Button>
+      <Button >Click me</Button>
+
+      
     </Box>
 
     <Box>
-      <FormControl
-       id="Id"
-        required
-        size="sm"
-        color="primary">
-        <FormLabel>
-          Label
-        </FormLabel>
-        <Input
-          placeholder="Placeholder"
-          name="Name"
-          type="tel"
-          autoComplete="on"
-          autoFocus
-          error
-          fullWidth
-          defaultValue="DefaultValue"
-          variant="outlined" />
-        <FormHelperText>
-          Help!
-        </FormHelperText>
-      </FormControl>
+
+<FormControl
+  id="Id"
+  required
+  size="sm"
+  color="primary">
+  <FormLabel>
+    Label
+  </FormLabel>
+  <Input
+    placeholder="Placeholder"
+    name="Name"
+    type="tel"
+    autoComplete="on"
+   autoFocus
+    error
+    fullWidth
+    defaultValue="DefaultValue"
+   variant="outlined" />
+  <FormHelperText>
+    Help!
+  </FormHelperText>
+</FormControl>
+
+
     </Box>
-    );
+      
+</Box>
+      
+  );
 }
