fd6d1c6557d6bb44eb72079a7faea27cb3c3d38b
DLBell217 <157739853+DLBell217@users.noreply.github.com>
2024-03-05 14:17:56 -0500

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -17,7 +17,7 @@ export default function BasicButtons() {
     </Box>
   );
 }
-<box>
+<Box>
 <FormControl
  id="Id"
   required
@@ -40,4 +40,4 @@ export default function BasicButtons() {
     Help!
   </FormHelperText>
 </FormControl>
-  </box>
+  </Box>
