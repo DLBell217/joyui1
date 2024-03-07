3e4c06950eccdea88358dc89b62f866937e9d2c2
DLBell217 <157739853+DLBell217@users.noreply.github.com>
2024-03-07 13:36:08 -0500

    Update Demo2.tsx

--- src/Demo2.tsx
+++ src/Demo2.tsx
@@ -5,9 +5,36 @@ import Box from '@mui/joy/Box';
 export default function BasicButtons() {
   return (
     <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
-      <Button>Button</Button>
-      <Button disabled>Disabled</Button>
-      <Button loading>Loading</Button>
+      <Button>Wow</Button>
+      <Button disabled>No way!</Button>
+      <Button loading>Thats cray!</Button>
     </Box>
+
+<Box>
+  <FormControl
+    id="Id"
+    required
+    size="sm"
+    color="primary">
+    <FormLabel>
+      Label
+    </FormLabel>
+    <Input
+      placeholder="Placeholder"
+      name="Name"
+      type="tel"
+      autoComplete="on"
+     autoFocus
+      error
+      fullWidth
+      defaultValue="DefaultValue"
+     variant="outlined" />
+    <FormHelperText>
+     WHY!!!!!!!!
+    </FormHelperText>
+  </FormControl>
+</Box>
+      
+</Box>
   );
 }
